3b83dad8bfaeb74097cc2642a613931946eab91f
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-10-04 10:42:44 +0900

    feat:log

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,18 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     pub fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+
+}
+#[wasm_bindgen]
+pub fn consolelog(name: &str) {
+    log(&format!("Hello, {}!", name));
+
 }
